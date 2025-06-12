b0c4ab1fdab3352bb50b74e2253bb2177bcda730
Fabio Brea <fabio.brea@gmail.com>
2025-06-12 12:20:59 +0200

    Update solution - CodeHub

--- 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.cs
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.cs
@@ -1,17 +1,9 @@
-using System.Collections.Generic;
+using System.Linq;
 ​
 public static class Kata
 {
   public static string AlphabetPosition(string text)
   {
-      var lower = text.ToLower();
-      List<int> chars = new();
-      foreach (var ch in lower)
-      {
-          var transformed = ch - 'a' + 1;
-          if (transformed <= 0 || transformed > 26) continue;
-          chars.Add(transformed);
-      }
-      return string.Join(" ", chars);
+    return string.Join(" ", text.ToLower().Where(char.IsLetter).Select(ch => ch - 'a' + 1));
   }
 }
