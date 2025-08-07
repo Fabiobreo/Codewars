97d66e9946a85f44e14774c309165d4dd6df47bc
Fabio Brea <fabio.brea@gmail.com>
2025-08-07 20:35:06 +0200

    Add solution - CodeHub

---
+++ 8-kyu/welcome/welcome.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+​
+public static class Kata
+    {
+        public static string Greet(string language)
+        {
+          var dict = new Dictionary<string, string>()
+          {
+            {"english", "Welcome"}, {"czech", "Vitejte"}, {"danish", "Velkomst"}, {"dutch", "Welkom"}, {"estonian", "Tere tulemast"}, {"finnish", "Tervetuloa"}, {"flemish", "Welgekomen"}, {"french", "Bienvenue"}, {"german", "Willkommen"}, {"irish", "Failte"}, {"italian", "Benvenuto"}, {"latvian", "Gaidits"}, {"lithuanian", "Laukiamas"}, {"polish", "Witamy"}, {"spanish", "Bienvenido"}, {"swedish", "Valkommen"}, {"welsh", "Croeso"}
+          };
+          if (dict.ContainsKey(language)) return dict[language];
+          return "Welcome";
+        }
+    }
