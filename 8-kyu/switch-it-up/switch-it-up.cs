086efc5426a2589a28290cc232488d51caad0a47
Fabio Brea <fabio.brea@gmail.com>
2025-08-11 13:12:56 +0200

    Add solution - CodeHub

---
+++ 8-kyu/switch-it-up/switch-it-up.cs
@@ -0,0 +1,17 @@
+public class Kata
+{
+  public static string SwitchItUp(int num)
+  {
+      if (num == 0) return "Zero";
+      if (num == 1) return "One";
+      if (num == 2) return "Two";
+      if (num == 3) return "Three";
+      if (num == 4) return "Four";
+      if (num == 5) return "Five";
+      if (num == 6) return "Six";
+      if (num == 7) return "Seven";
+      if (num == 8) return "Eight";
+      if (num == 9) return "Nine";
+      return "idk";
+  }
+}
