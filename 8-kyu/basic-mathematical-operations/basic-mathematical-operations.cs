251dc99f2283d005eea17cd4d2410b69f0c6f74a
Fabio Brea <fabio.brea@gmail.com>
2025-09-14 10:03:04 +0200

    Add solution - CodeHub

---
+++ 8-kyu/basic-mathematical-operations/basic-mathematical-operations.cs
@@ -0,0 +1,26 @@
+namespace Solution
+{
+  public static class Program
+  {
+    public static double basicOp(char operation, double value1, double value2)
+    {
+        if (operation == '+')
+        {
+            return value1 + value2;
+        }
+        else if (operation == '-')
+        {
+            return value1 - value2;
+        }
+        else if (operation == '*')
+        {
+            return value1 * value2;
+        }
+        else if (operation == '/')
+        {
+            return value1 / value2;
+        }
+        return -1;
+    }
+  }
+}
