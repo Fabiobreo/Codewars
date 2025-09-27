176adca62beb43cc5ca696f82c1234a96ad3973a
Fabio Brea <fabio.brea@gmail.com>
2025-09-27 11:54:38 +0200

    Add solution - CodeHub

---
+++ 7-kyu/list-filtering/list-filtering.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System.Linq;
+​
+public class ListFilterer
+{
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+    {
+        return listOfItems?.OfType<int>() ?? Enumerable.Empty<int>();
+    }
+}
