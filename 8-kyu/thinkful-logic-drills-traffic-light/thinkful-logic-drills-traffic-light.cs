2829d23d9072b0035138cd859cf9b315987a9cb2
Fabio Brea <fabio.brea@gmail.com>
2025-10-18 09:48:44 +0200

    Add solution - CodeHub

---
+++ 8-kyu/thinkful-logic-drills-traffic-light/thinkful-logic-drills-traffic-light.cs
@@ -0,0 +1,13 @@
+public class Kata
+{
+    public static string UpdateLight(string current)
+    {
+        switch (current)
+        {
+            case "green": return "yellow";
+            case "yellow": return "red";
+            case "red": return "green";
+        }
+        return "";
+    }
+}
