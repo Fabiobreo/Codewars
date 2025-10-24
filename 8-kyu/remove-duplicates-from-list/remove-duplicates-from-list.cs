a15a5a23a8da4e1a2a3548588cd99f4749ae3931
Fabio Brea <fabio.brea@gmail.com>
2025-10-24 15:00:00 +0200

    Add solution - CodeHub

---
+++ 8-kyu/remove-duplicates-from-list/remove-duplicates-from-list.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+​
+namespace Solution
+{
+  public static class Program
+  {
+    public static int[] distinct(int[] a)
+    {
+        return a.Distinct().ToArray();
+    }
+  }
+}
