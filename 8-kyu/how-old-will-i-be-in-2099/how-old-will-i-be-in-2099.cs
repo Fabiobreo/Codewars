1d0ed24fee6fc61838aa42f0ab1eea606d27e4ef
Fabio Brea <fabio.brea@gmail.com>
2025-11-08 10:21:13 +0100

    Add solution - CodeHub

---
+++ 8-kyu/how-old-will-i-be-in-2099/how-old-will-i-be-in-2099.cs
@@ -0,0 +1,15 @@
+using System;
+​
+public static class AgeDiff 
+{
+  public static string CalculateAge(int birth, int yearTo) 
+  {
+    int age = yearTo - birth;
+    if (age > 1) return $"You are {age} years old.";
+    if (age == 1) return "You are 1 year old.";
+    if (age == 0) return "You were born this very year!";
+    if (age == -1) return "You will be born in 1 year.";
+    if (age < -1) return $"You will be born in {age * -1} years.";
+    return "";
+  }
+}
