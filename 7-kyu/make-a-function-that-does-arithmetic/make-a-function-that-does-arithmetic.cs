907f559a6c1b6f95bdad857c04e9e5d6f114d663
Fabio Brea <fabio.brea@gmail.com>
2025-11-24 14:03:54 +0100

    Add solution - CodeHub

---
+++ 7-kyu/make-a-function-that-does-arithmetic/make-a-function-that-does-arithmetic.cs
@@ -0,0 +1,20 @@
+using System;
+​
+public class Kata 
+{
+  public static double Arithmetic(double a, double b, string op) 
+  {
+      switch (op)
+      {
+          case "add":
+            return a + b;
+          case "subtract":
+            return a - b;
+          case "multiply":
+            return a * b;
+          case "divide":
+            return a / b;
+      }
+      return 0;
+  }
+}
